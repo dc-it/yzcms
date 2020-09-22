050a7b3aa99c109bd1a6d23aa863ac7165a58b95
duchaoyun <1255604509@qq.com>
2020-09-22 20:51:58 +0800

    sql

--- doc/db.sql
+++ doc/db.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80021
  File Encoding         : 65001
 
- Date: 21/09/2020 17:03:55
+ Date: 22/09/2020 20:50:51
 */
 
 SET NAMES utf8mb4;
@@ -21,111 +21,212 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for cms_article
 -- ----------------------------
 DROP TABLE IF EXISTS `cms_article`;
-CREATE TABLE `cms_article`  (
-  `id` bigint(0) NOT NULL,
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `cms_article`
+(
+    `id`          bigint(0)                                               NOT NULL,
+    `titile`      varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '标题',
+    `keywords`    varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '关键字',
+    `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '描述',
+    `imageId`     bigint(0)                                               NULL DEFAULT NULL COMMENT '缩略图id',
+    `author`      varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '作者',
+    `type`        int(0)                                                  NULL DEFAULT NULL COMMENT '1->推荐，2->置顶',
+    `menuId`      bigint(0)                                               NULL DEFAULT NULL COMMENT '菜单id',
+    `websiteId`   bigint(0)                                               NULL DEFAULT NULL COMMENT '网站id',
+    `pageview`    int(0)                                                  NULL DEFAULT 0 COMMENT '阅读数',
+    `check`       bit(1)                                                  NULL DEFAULT NULL COMMENT '是否审核',
+    `create_time` datetime(0)                                             NULL DEFAULT NULL COMMENT '创建时间',
+    `update_time` datetime(0)                                             NULL DEFAULT NULL COMMENT '更新时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for cms_article_content
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_article_content`;
+CREATE TABLE `cms_article_content`
+(
+    `id`          bigint(0)                                       NOT NULL COMMENT '主键',
+    `article_id`  bigint(0)                                       NOT NULL COMMENT '文章id',
+    `menu_id`     bigint(0)                                       NOT NULL COMMENT '菜单id',
+    `website_id`  bigint(0)                                       NOT NULL COMMENT '网站id',
+    `content`     text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '内容',
+    `create_time` datetime(0)                                     NULL DEFAULT NULL COMMENT '创建时间',
+    `update_time` datetime(0)                                     NULL DEFAULT NULL COMMENT '更新时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for cms_article_tag_relation
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_article_tag_relation`;
+CREATE TABLE `cms_article_tag_relation`
+(
+    `id`          bigint(0)   NOT NULL,
+    `article_id`  bigint(0)   NOT NULL COMMENT '文章id',
+    `tag_id`      bigint(0)   NOT NULL COMMENT '标签id',
+    `website_id`  bigint(0)   NOT NULL COMMENT '网站id',
+    `create_time` datetime(0) NOT NULL COMMENT '创建时间',
+    `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for cms_menu
 -- ----------------------------
 DROP TABLE IF EXISTS `cms_menu`;
-CREATE TABLE `cms_menu`  (
-  `id` bigint(0) NOT NULL,
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `cms_menu`
+(
+    `id`          bigint(0)   NOT NULL,
+    `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+    `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for cms_tag
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_tag`;
+CREATE TABLE `cms_tag`
+(
+    `id`          bigint(0)                                               NOT NULL COMMENT 'tag id',
+    `name`        varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '名称',
+    `sort`        int(0)                                                  NULL DEFAULT NULL COMMENT '排序',
+    `article_num` int(0)                                                  NULL DEFAULT NULL COMMENT '文章数量',
+    `website_id`  bigint(0)                                               NULL DEFAULT NULL COMMENT '所属网站id',
+    `create_time` datetime(0)                                             NULL DEFAULT NULL COMMENT '创建时间',
+    `update_time` datetime(0)                                             NULL DEFAULT NULL COMMENT '更新时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for sys_permission
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_permission`;
-CREATE TABLE `sys_permission`  (
-  `id` bigint(0) NOT NULL,
-  `pid` bigint(0) NULL DEFAULT NULL COMMENT '父级权限id',
-  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '名称',
-  `value` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '权限值',
-  `icon` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '图标',
-  `type` int(0) NULL DEFAULT NULL COMMENT '权限类型：0->目录；1->菜单；2->按钮（接口绑定权限）',
-  `uri` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '前端资源路径',
-  `status` int(0) NULL DEFAULT NULL COMMENT '启用状态；0->禁用；1->启用',
-  `sort` int(0) NULL DEFAULT NULL COMMENT '排序',
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `sys_permission`
+(
+    `id`          bigint(0)                                               NOT NULL,
+    `pid`         bigint(0)                                               NULL DEFAULT NULL COMMENT '父级权限id',
+    `name`        varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '名称',
+    `value`       varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '权限值',
+    `icon`        varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '图标',
+    `type`        int(0)                                                  NULL DEFAULT NULL COMMENT '权限类型：0->目录；1->菜单；2->按钮（接口绑定权限）',
+    `uri`         varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '前端资源路径',
+    `status`      int(0)                                                  NULL DEFAULT NULL COMMENT '启用状态；0->禁用；1->启用',
+    `sort`        int(0)                                                  NULL DEFAULT NULL COMMENT '排序',
+    `create_time` datetime(0)                                             NULL DEFAULT NULL COMMENT '创建时间',
+    `update_time` datetime(0)                                             NULL DEFAULT NULL COMMENT '更新时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for sys_role
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_role`;
-CREATE TABLE `sys_role`  (
-  `id` bigint(0) NOT NULL,
-  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '名称',
-  `description` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '描述',
-  `admin_count` int(0) NULL DEFAULT NULL COMMENT '后台用户数量',
-  `sort` int(0) NULL DEFAULT 0,
-  `status` int(0) NULL DEFAULT 1 COMMENT '启用状态：0->禁用；1->启用',
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `sys_role`
+(
+    `id`          bigint(0)                                               NOT NULL,
+    `name`        varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '名称',
+    `description` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '描述',
+    `admin_count` int(0)                                                  NULL DEFAULT NULL COMMENT '后台用户数量',
+    `sort`        int(0)                                                  NULL DEFAULT 0,
+    `status`      int(0)                                                  NULL DEFAULT 1 COMMENT '启用状态：0->禁用；1->启用',
+    `create_time` datetime(0)                                             NULL DEFAULT NULL COMMENT '创建时间',
+    `update_time` datetime(0)                                             NULL DEFAULT NULL COMMENT '更新时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for sys_role_permission_relation
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_role_permission_relation`;
-CREATE TABLE `sys_role_permission_relation`  (
-  `id` bigint(0) NOT NULL AUTO_INCREMENT,
-  `role_id` bigint(0) NULL DEFAULT NULL,
-  `permission_id` bigint(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 18 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '后台用户角色和权限关系表' ROW_FORMAT = Dynamic;
+CREATE TABLE `sys_role_permission_relation`
+(
+    `id`            bigint(0) NOT NULL AUTO_INCREMENT,
+    `role_id`       bigint(0) NULL DEFAULT NULL,
+    `permission_id` bigint(0) NULL DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 18
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci COMMENT = '后台用户角色和权限关系表'
+  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for sys_user
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user`;
-CREATE TABLE `sys_user`  (
-  `id` bigint(0) NOT NULL,
-  `username` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `password` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `icon` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '头像',
-  `phone` varchar(13) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号',
-  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
-  `nick_name` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '昵称',
-  `remark` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注信息',
-  `login_time` datetime(0) NULL DEFAULT NULL COMMENT '最后登录时间',
-  `status` bit(1) NULL DEFAULT b'1' COMMENT '帐号启用状态：0->禁用；1->启用',
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `sys_user`
+(
+    `id`          bigint(0)                                               NOT NULL,
+    `username`    varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL DEFAULT NULL,
+    `password`    varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL DEFAULT NULL,
+    `icon`        varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '头像',
+    `phone`       varchar(13) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL DEFAULT NULL COMMENT '手机号',
+    `email`       varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
+    `nick_name`   varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '昵称',
+    `remark`      varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注信息',
+    `login_time`  datetime(0)                                             NULL DEFAULT NULL COMMENT '最后登录时间',
+    `status`      bit(1)                                                  NULL DEFAULT b'1' COMMENT '帐号启用状态：0->禁用；1->启用',
+    `create_time` datetime(0)                                             NULL DEFAULT NULL COMMENT '创建时间',
+    `update_time` datetime(0)                                             NULL DEFAULT NULL COMMENT '更新时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for sys_user_permission_relation
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user_permission_relation`;
-CREATE TABLE `sys_user_permission_relation`  (
-  `id` bigint(0) NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(0) NULL DEFAULT NULL,
-  `permission_id` bigint(0) NULL DEFAULT NULL,
-  `own` bit(1) NULL DEFAULT NULL COMMENT '是否有此权限，补充角色权限',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '后台用户和权限关系表(除角色中定义的权限以外的加减权限)' ROW_FORMAT = Dynamic;
+CREATE TABLE `sys_user_permission_relation`
+(
+    `id`            bigint(0) NOT NULL AUTO_INCREMENT,
+    `user_id`       bigint(0) NULL DEFAULT NULL,
+    `permission_id` bigint(0) NULL DEFAULT NULL,
+    `own`           bit(1)    NULL DEFAULT NULL COMMENT '是否有此权限，补充角色权限',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci COMMENT = '后台用户和权限关系表(除角色中定义的权限以外的加减权限)'
+  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for sys_user_role_relation
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user_role_relation`;
-CREATE TABLE `sys_user_role_relation`  (
-  `id` bigint(0) NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(0) NULL DEFAULT NULL,
-  `role_id` bigint(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '后台用户和角色关系表' ROW_FORMAT = Dynamic;
+CREATE TABLE `sys_user_role_relation`
+(
+    `id`      bigint(0) NOT NULL AUTO_INCREMENT,
+    `user_id` bigint(0) NULL DEFAULT NULL,
+    `role_id` bigint(0) NULL DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 17
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci COMMENT = '后台用户和角色关系表'
+  ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
